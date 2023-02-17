bd6da506abfc44b5edc1dad0b88fa0647f0ffc36
seif <116313275+sahnoune1@users.noreply.github.com>
2023-02-17 18:33:39 +0100

    dist/main.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ let formId = 1;
 let array = [];
 let currentProject = null;
 let pages = {};
+let test = [];
+let store;
 /*create the add event*/
 add.addEventListener("click", () => {
   const form = document.createElement("form");
@@ -36,8 +38,16 @@ add.addEventListener("click", () => {
   /*create the event for the first submit*/
   fo.addEventListener("submit", (event) => {
     event.preventDefault();
-    const store = titleInput.value;
+    store = titleInput.value;
+    if (test.includes(store)) {
+      alert("type again");
+      return;
+    }
+
+    test.push(store);
     console.log(store);
+    console.log(test);
+
     const list = document.querySelector(".list");
     const div = document.createElement("div");
     div.setAttribute("class", "last");
@@ -48,34 +58,46 @@ add.addEventListener("click", () => {
     console.log(array);
     const elements = document.querySelectorAll(".last");
     console.log(elements);
+    let projectInputs = [];
     for (let i = 0; i < array.length; i++) {
       elements[i].addEventListener("click", () => {
         const page = document.createElement("div");
-        page.innerHTML = array[i].innerHTML;
+        page.setAttribute("class", "page");
+
+        const head = document.createElement("div");
+        head.setAttribute("class", "head");
+        head.innerHTML = `project${i}`;
+
+        const middle = document.createElement("div");
+        middle.setAttribute("class", "middle");
+
+        const lol = document.createElement("div");
+        lol.setAttribute("class", "lol");
+
+        lol.innerHTML = projectInputs[i] || "";
+
+        const foot = document.createElement("div");
+        foot.setAttribute("class", "foot");
+
+        middle.appendChild(lol);
+        page.appendChild(head);
+        page.appendChild(middle);
+        page.appendChild(foot);
         main.appendChild(page);
+
         if (currentProject !== null) {
           currentProject.style.display = "none";
         }
         page.style.display = "block";
         currentProject = page;
+
+        head.addEventListener("click", () => {
+          lol.innerHTML = `seif${i}`;
+
+          projectInputs[i] = lol.innerHTML;
+        });
       });
     }
-    // for (let i = 0; i < array.length; i++) {
-    //   array[i].addEventListener("click", () => {
-    //     currentProject = null;
-    //     const page = document.createElement("div");
-    //     page.setAttribute("class", `page${i}`);
-    //     console.log(`page${i}`);
-    //     page.innerHTML = i;
-    //     main.appendChild(page);
-    //     if (currentProject !== null) {
-    //       currentProject.style.display = "none";
-    //     }
-    //     const newPage = document.querySelector(`.page${i}`);
-    //     newPage.style.display = "block";
-    //     currentProject = newPage;
-    //   });
-    // }
   });
   formId++;
 });
