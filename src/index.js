a05445d86a0f40b5639b196896afe87a82beb0fb
seif <116313275+sahnoune1@users.noreply.github.com>
2023-02-21 19:06:51 +0100

    dist/index.html

--- src/index.js
+++ src/index.js
@@ -127,13 +127,13 @@ add.addEventListener("click", () => {
                 formm.appendChild(titleInput2);
 
                 const titleLabel3 = document.createElement("label");
-                titleLabel3.setAttribute("for", "title");
+                titleLabel3.setAttribute("for", "date");
                 formm.appendChild(titleLabel3);
 
                 const titleInput3 = document.createElement("input");
-                titleInput3.setAttribute("type", "text");
-                titleInput3.setAttribute("id", "titles");
-                titleInput3.setAttribute("name", "title");
+                titleInput3.setAttribute("type", "date");
+                titleInput3.setAttribute("id", "date");
+                titleInput3.setAttribute("name", "date");
                 titleInput3.setAttribute("placeholder", "Date");
                 titleInput3.setAttribute("required", true);
                 formm.appendChild(titleInput3);
@@ -162,6 +162,9 @@ add.addEventListener("click", () => {
                   const foot4 = document.createElement("button");
                   foot4.setAttribute("class", "foot4");
                   foot4.innerHTML = "delete";
+                  const foot5 = document.createElement("button");
+                  foot5.setAttribute("class", "foot4");
+                  foot5.innerHTML = "EDIT";
 
                   const foott = document.createElement("div");
                   foott.classList.add("fat", `fat${i + j}`);
@@ -173,6 +176,7 @@ add.addEventListener("click", () => {
                   foott.appendChild(foot2);
                   foott.appendChild(foot3);
                   foott.appendChild(foot4);
+                  foott.appendChild(foot5);
                   foot.appendChild(foott);
 
                   change1 = titleInput1.value;
@@ -193,6 +197,121 @@ add.addEventListener("click", () => {
 
                   foot4.addEventListener("click", () => {
                     foott.remove();
+                  });
+
+                  function edit() {
+                    foott.remove();
+                    if (!projectInputs[i]) {
+                      const formm = document.createElement("form");
+                      formm.setAttribute("class", `formMiddle form${i}`);
+                      middle.appendChild(formm);
+
+                      const titleLabel1 = document.createElement("label");
+                      titleLabel1.setAttribute("for", "title");
+                      formm.appendChild(titleLabel1);
+
+                      const titleInput1 = document.createElement("input");
+                      titleInput1.setAttribute("type", "text");
+                      titleInput1.setAttribute("id", "titles");
+                      titleInput1.setAttribute("name", "title");
+                      titleInput1.setAttribute("placeholder", "title");
+                      titleInput1.setAttribute("required", true);
+                      formm.appendChild(titleInput1);
+
+                      const titleLabel2 = document.createElement("label");
+                      titleLabel2.setAttribute("for", "title");
+                      formm.appendChild(titleLabel2);
+
+                      const titleInput2 = document.createElement("input");
+                      titleInput2.setAttribute("type", "text");
+                      titleInput2.setAttribute("id", "titless");
+                      titleInput2.setAttribute("name", "title");
+                      titleInput2.setAttribute(
+                        "placeholder",
+                        "Description of the project "
+                      );
+                      titleInput2.setAttribute("required", true);
+                      formm.appendChild(titleInput2);
+
+                      const titleLabel3 = document.createElement("label");
+                      titleLabel3.setAttribute("for", "date");
+                      formm.appendChild(titleLabel3);
+
+                      const titleInput3 = document.createElement("input");
+                      titleInput3.setAttribute("type", "date");
+                      titleInput3.setAttribute("id", "date");
+                      titleInput3.setAttribute("name", "date");
+                      titleInput3.setAttribute("placeholder", "Date");
+                      titleInput3.setAttribute("required", true);
+                      formm.appendChild(titleInput3);
+
+                      const submitLabel = document.createElement("label");
+                      submitLabel.setAttribute("for", "submit");
+                      formm.appendChild(submitLabel);
+                      const submitInput = document.createElement("input");
+                      submitInput.setAttribute("type", "submit");
+                      submitInput.setAttribute("id", "submits");
+                      submitInput.setAttribute("value", "submit");
+
+                      formm.appendChild(submitInput);
+
+                      projectInputs[i] = formm;
+                      console.log(projectInputs[i]);
+                      const existingValues = foott.querySelectorAll("div");
+
+                      // Populate the form fields with the existing values
+                      titleInput1.value = existingValues[0].innerHTML;
+                      titleInput2.value = existingValues[1].innerHTML;
+                      titleInput3.value = existingValues[2].innerHTML;
+                      formm.addEventListener("submit", (event) => {
+                        event.preventDefault();
+
+                        const foot1 = document.createElement("div");
+                        foot1.setAttribute("class", "foot1");
+                        const foot2 = document.createElement("div");
+                        foot2.setAttribute("class", "foot2");
+                        const foot3 = document.createElement("div");
+                        foot3.setAttribute("class", "foot3");
+                        const foot4 = document.createElement("button");
+                        foot4.setAttribute("class", "foot4");
+                        foot4.innerHTML = "delete";
+                        const foot5 = document.createElement("button");
+                        foot5.setAttribute("class", "foot4");
+                        foot5.innerHTML = "EDIT";
+
+                        const foott = document.createElement("div");
+                        foott.classList.add("fat", `fat${i + j}`);
+                        // const fat = document.querySelector(`.fat${i + j}`);
+                        // console.log(foott);
+                        // console.log(fat);
+
+                        foott.appendChild(foot1);
+                        foott.appendChild(foot2);
+                        foott.appendChild(foot3);
+                        foott.appendChild(foot4);
+                        foott.appendChild(foot5);
+                        foot.appendChild(foott);
+
+                        change1 = titleInput1.value;
+                        foot1.innerHTML = change1;
+
+                        change2 = titleInput2.value;
+                        foot2.innerHTML = change2;
+
+                        change3 = titleInput3.value;
+                        foot3.innerHTML = change3;
+
+                        titleInput1.value = "";
+                        titleInput2.value = "";
+                        titleInput3.value = "";
+                        middle.removeChild(formm);
+                        projectInputs = [];
+                        j++;
+                      });
+                    }
+                  }
+                  foot5.addEventListener("click", () => {
+                    edit();
                   });
                 });
               }
