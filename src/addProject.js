a87471e54e38c5586e9ac3112942e6f065830d25
seif <116313275+sahnoune1@users.noreply.github.com>
2023-02-25 15:43:59 +0100

    dist/main.js

--- src/addProject.js
+++ src/addProject.js
@@ -1,40 +1,40 @@
-import { createMain } from "./createMain";
+// import { createMain } from "./createMain";
 
-function addProject(element, i, projectInputs, main, change1, currentProject) {
-  element.addEventListener("click", () => {
-    let page = document.querySelector(`.page.project-${i}`);
+// function addProject(element, i, projectInputs, main, change1, currentProject) {
+//   element.addEventListener("click", () => {
+//     let page = document.querySelector(`.page.project-${i}`);
 
-    if (!page) {
-      // Create a new page element
-      page = document.createElement("div");
-      page.setAttribute("class", `page project-${i}`);
+//     if (!page) {
+//       // Create a new page element
+//       page = document.createElement("div");
+//       page.setAttribute("class", `page project-${i}`);
 
-      const head = document.createElement("div");
-      head.setAttribute("class", "head");
-      head.innerHTML = `project${i}`;
+//       const head = document.createElement("div");
+//       head.setAttribute("class", "head");
+//       head.innerHTML = `project${i}`;
 
-      const middle = document.createElement("div");
-      middle.setAttribute("class", "middle");
+//       const middle = document.createElement("div");
+//       middle.setAttribute("class", "middle");
 
-      const foot = document.createElement("div");
-      foot.setAttribute("class", "foot");
+//       const foot = document.createElement("div");
+//       foot.setAttribute("class", "foot");
 
-      page.appendChild(head);
-      page.appendChild(middle);
-      page.appendChild(foot);
-      main.appendChild(page);
+//       page.appendChild(head);
+//       page.appendChild(middle);
+//       page.appendChild(foot);
+//       main.appendChild(page);
 
-      // Add event listeners
-      createMain(head, projectInputs, i, middle, foot, change1);
-    }
+//       // Add event listeners
+//       createMain(head, projectInputs, i, middle, foot, change1);
+//     }
 
-    // Hide current project and show current one
-    if (currentProject !== null) {
-      currentProject.style.display = "none";
-    }
-    page.style.display = "block";
-    currentProject = page;
-  });
-}
+//     // Hide current project and show current one
+//     if (currentProject !== null) {
+//       currentProject.style.display = "none";
+//     }
+//     page.style.display = "block";
+//     currentProject = page;
+//   });
+// }
 
-export { addProject };
+// export { addProject };
--- src/firstSubmit.js
+++ src/firstSubmit.js
@@ -1,4 +1,4 @@
-import { addProject } from "./addProject";
+import { createMain } from "./createMain";
 
 function firstSubmit(
   form,
@@ -43,7 +43,40 @@ function firstSubmit(
     let projectInputs = [];
 
     for (let i = 0; i < elements.length; i++) {
-      addProject(elements[i], i, projectInputs, main, change1, currentProject);
+      elements[i].addEventListener("click", () => {
+        let page = document.querySelector(`.page.project-${i}`);
+
+        if (!page) {
+          // Create a new page element
+          page = document.createElement("div");
+          page.setAttribute("class", `page project-${i}`);
+
+          const head = document.createElement("div");
+          head.setAttribute("class", "head");
+          head.innerHTML = `project${i}`;
+
+          const middle = document.createElement("div");
+          middle.setAttribute("class", "middle");
+
+          const foot = document.createElement("div");
+          foot.setAttribute("class", "foot");
+
+          page.appendChild(head);
+          page.appendChild(middle);
+          page.appendChild(foot);
+          main.appendChild(page);
+
+          // Add event listeners
+          createMain(head, projectInputs, i, middle, foot, change1);
+        }
+
+        // Hide current project and show current one
+        if (currentProject !== null) {
+          currentProject.style.display = "none";
+        }
+        page.style.display = "block";
+        currentProject = page;
+      });
     }
 
     // Save the updated list content to local storage
